8891c77f1b9812126bb63187f5a71d3aef737e71
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 01:32:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/[0-9]/).length == 0 
+  if phone.scan(/[\d()-]/).length == 0 
     return false
   else
     return true
