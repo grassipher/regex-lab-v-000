3f38d54e035bf130db1a107db6bde6b6f4b62994
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 01:20:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]\w/) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z].*\W/).length == 0
+    return false
+  else
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
-
-end
+  if phone.scan(//).length == 0 
+    return false
+  else
+    return true
+  end
+end #"2438894546", "(718)891-1313", "234 435 9978", "(800)4261134"
